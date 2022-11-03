4e73a40824ddf342b6c157a4a0b0540bdcb32416
jkarenzi <j.karenzi@alustudent.com>
2022-11-03 16:42:45 +0200

    added 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        for key, val in a_dictionary.items():
-            if val == max(a_dictionary.values()):
-                return key
-            else:
-                return None
+    if a_dictionary:
+        return sorted(a_dictionary, key=a_dictionary.get, reverse=True)[0]
+    return None
