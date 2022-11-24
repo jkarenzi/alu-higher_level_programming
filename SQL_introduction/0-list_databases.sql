894915382847604c1c647c50be8d664672cab83e
jkarenzi <j.karenzi@alustudent.com>
2022-11-24 07:53:15 +0200

    added files

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- script that lists all the databases on  MySQL server --
+-- script that lists all the databases on  MySQL server --
+SHOW DATABASES;
---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- listing by best
+SELECT score, name FROM second_table 
+ORDER BY score DESC;
